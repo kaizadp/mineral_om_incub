0cf4cde877cc919536890b569105361487bd0447
Kaizad <kaizad.patel@pnnl.gov>
2020-07-06 09:50:02 -0700

    update NOSC. add Gibbs, DBE, KMD indices. set mean Mass/KM/KMD for formulas with double values.

--- code/fticr_processing.R
+++ code/fticr_processing.R
@@ -6,6 +6,7 @@
 
 source("code/0-packages.R")
 
+
 # 1. load report ----------------------------------------------------------
 corekey = read.csv("data/corekey.csv")
 report = read.csv("data/Bioavailability_Incubation_LDRD_ICR_Report.csv")
@@ -26,9 +27,14 @@ fticr_meta_temp =
   dplyr::select(Mass:Class) %>% 
   # create columns for indices
   dplyr::mutate(AImod = round((1+C-(0.5*O)-S-(0.5*(N+P+H)))/(C-(0.5*O)-S-N-P),4),
-                NOSC =  round(4-(((4*C)+H-(3*N)-(2*O)-(2*S))/C),4),
+                NOSC =  round(4-(((4*C)+H-(3*N)-(2*O)-(2*S)+(5*P))/C),4),
+                Gibbs = 60.3-(28.5*NOSC),
                 HC = round(H/C,2),
-                OC = round(O/C,2)) %>% 
+                OC = round(O/C,2),
+                DBE_AI = 1+C-O-S-0.5*(N+P+H),
+                DBE_C = DBE_AI/C,
+                KM = Mass * (14/14.01565),
+                KMD = as.integer(KM)-KM) %>% 
   # create column/s for formula
   # first, create columns for individual elements
   # then, combine
@@ -40,16 +46,26 @@ fticr_meta_temp =
                 formula_p = if_else(P>0,paste0("P",P),as.character(NA)),
                 formula = paste0(formula_c,formula_h, formula_o, formula_n, formula_s, formula_p),
                 formula = str_replace_all(formula,"NA","")) %>% 
-  dplyr::select(Mass, formula, El_comp, Class, HC, OC, AImod, NOSC, C:P) 
+  dplyr::select(Mass, formula, El_comp, Class, HC, OC, AImod, NOSC, Gibbs, DBE_AI, DBE_C, KM, KMD, C:P) 
 
 fticr_meta_mass_formula  =
   fticr_meta_temp %>% 
   select(Mass, formula)
 
+fticr_meta_mass_formula_kmd  =
+  fticr_meta_temp %>% 
+  select(Mass, formula, KM, KMD) %>% 
+  group_by(formula) %>% 
+  summarise(Mass = round(mean(Mass),4),
+            KM = round(mean(KM),4),
+            KMD = round(mean(KMD),4))
+
 fticr_meta = 
   fticr_meta_temp %>% 
-  select(-Mass) %>% 
-  distinct()
+  select(-Mass, -KM, -KMD) %>% 
+  distinct() %>% 
+  left_join(fticr_meta_mass_formula_kmd, by = "formula") %>% 
+  select(Mass, formula:DBE_C, KM, KMD, C:P)
 
 # subset of meta for HC/OC only, for Van Krevelen diagrams
 meta_hcoc = 
@@ -79,14 +95,16 @@ data_long2  =
   na.omit() %>% 
   # include only peaks seen in all replicates
   group_by(Pre_post, Temperature, Moisture, Clay, formula) %>% 
-  dplyr::mutate(n = n())
+  dplyr::mutate(n = n()) %>% 
+  filter(n>1) %>% 
+  select(-n)
 
 #
 # 4. OUTPUT -----------------------------------------------------------------
 
-write.csv(fticr_meta, "processed/fticr_meta.csv", row.names = F)
-write.csv(data_long2, "processed/fticr_data_long.csv", row.names = F)
-write.csv(meta_hcoc, "processed/fticr_meta_hcoc.csv", row.names = F)
+write.csv(fticr_meta, "data/processed/fticr_meta.csv", row.names = F)
+write.csv(data_long2, "data/processed/fticr_data_long.csv", row.names = F)
+write.csv(meta_hcoc, "data/processed/fticr_meta_hcoc.csv", row.names = F)
 
 
 
